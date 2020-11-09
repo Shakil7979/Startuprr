875a4c6bde9b846bf6dd608190f385a54cad6acf
Shakil7979 <71561507+Shakil7979@users.noreply.github.com>
2020-11-09 13:37:07 +0600

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,72 @@
+ $(window).ready(function(){
+ 	$('.skill-bar').pieChart({
+ 		barColor: '#1CA399',
+ 		trackColor: '#fff',
+ 		lineWidth: 3,
+ 		size: 100,
+ 		animate: { 
+				  duration: 1000, 
+				  enabled: true 
+				},
+ 	});
+
+ 	// =====counter up 
+ 	$('.counter p').counterUp({
+	 	time:1000  
+	 });
+
+ 	// ======testimonials owl carousel 
+ 	$('.testimonials').owlCarousel({
+ 		items:1,
+ 		nav:true,
+ 		dots:false,
+ 		autoplay:true,
+ 		loop:true,
+ 	})
+ 	// ======company owl carousel 
+ 	$('.company').owlCarousel({
+ 		items:4,
+ 		nav:true,
+ 		dots:false,
+ 		autoplay:true,
+ 		loop:true,
+ 		responsive:{
+	        0:{
+	            items:1
+	        },
+	        576:{
+	            items:2
+	        },
+	        767:{
+	            items:3
+	        }
+	    }
+ 	})
+
+ 	//====== menu clik scroll
+ 	$('.menu ul li a').click(function(){
+		$('html,body').animate({
+			scrollTop:$($(this).attr('href')).offset().top
+		},500)
+
+		return false;
+	})
+	// ====mobile menu 
+	$('.nav-icon').click(function(){
+		$('.menu ul').slideToggle(1000);
+
+		return false;
+	}) 
+ 	
+ }) 
+
+ // =======FixedHeader
+ $(window).scroll(function(){
+	var value=$(this).scrollTop()
+
+	if(value>600){
+		$('.header-area').addClass('FixedHeader')
+	}else{
+		$('.header-area').removeClass('FixedHeader')
+	}
+})
